541cc9c58a22dbf8f0967d3e9414c5fbeafb4858
shroukwagdysayed <158518690+shroukwagdysayed@users.noreply.github.com>
2024-12-20 18:58:15 +0200

    commit

--- resources/views/courseplay.blade.php
+++ resources/views/courseplay.blade.php
@@ -1,38 +1,35 @@
 @extends("layouts.master")
 @section('title', "Course Play")
 @section('styles')
-    <link rel="stylesheet" href="{{asset("css/courseplay.css")}}"
+    <link rel="stylesheet" href="{{asset("css/courseplay.css")}}">
 @endsection
 @section('content')
     <section class="main">
         <div class="main-video">
-            <video controls>
+            <video id="main-video" controls>
                 <source src="{{$videos[0]->path}}" type="video/mp4">
             </video>
-            <div class="title">Episode Title</div>
+            <div class="title" id="main-title">Episode Title</div>
         </div>
         <div class="playlist">
             @foreach($videos as $video)
-                <div class="playlist-item">
-                    <a href="#">
+                <div class="playlist-item" onclick="changeVideo('{{$video->path}}', '{{$video->title}}')">
                     <video muted>
                         <source src="{{$video->path}}" type="video/mp4">
                     </video>
                     <div class="title">{{$video->title}}</div>
-                    </a>
                 </div>
             @endforeach
         </div>
     </section>
 
     <script>
-            function changeVideo(videoPath, videoTitle) {
+        function changeVideo(videoPath, videoTitle) {
             var mainVideo = document.getElementById('main-video');
             var mainTitle = document.getElementById('main-title');
             mainVideo.src = videoPath;
             mainTitle.textContent = videoTitle;
-            mainVideo.play();       
-          }
+            mainVideo.play(); 
+        }
     </script>
-
 @endsection
