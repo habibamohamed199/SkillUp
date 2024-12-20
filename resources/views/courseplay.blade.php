2b23a60c40dc536bda86beade9beeee5b26e5c9c
shroukwagdysayed <158518690+shroukwagdysayed@users.noreply.github.com>
2024-12-20 17:27:54 +0200

    commit

---
+++ resources/views/courseplay.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video Playlist</title>
+    <link rel="stylesheet" href="courseplay.css">
+</head>
+<body>
+    <div class="main-video">
+        <video controls>
+            <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+        </video>
+        <div class="title">Episode Title</div>
+    </div>
+    <div class="playlist">
+        <div class="playlist-item">
+            <video muted>
+                <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+            </video>
+            <div class="title">Episode Title</div>
+        </div>
+        <div class="playlist-item">
+            <video muted>
+                <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+            </video>
+            <div class="title">Episode Title</div>
+        </div>
+        <div class="playlist-item">
+            <video muted>
+                <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+            </video>
+            <div class="title">Episode Title</div>
+        </div>
+        <div class="playlist-item">
+            <video muted>
+                <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+            </video>
+            <div class="title">Episode Title</div>
+        </div>
+        <div class="playlist-item">
+            <video muted>
+                <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+            </video>
+            <div class="title">Episode Title</div>
+        </div>
+        <div class="playlist-item">
+            <video muted>
+                <source src="videos\Lecture 03_ Part 01- Introduction to PHP.mp4" type="video/mp4">
+            </video>
+            <div class="title">Episode Title</div>
+        </div>
+    </div>
+</body>
+</html>
